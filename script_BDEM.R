8cc56b8f943e819c6a445a7f0ae2b3338946f383
Joao <joaolimab773@gmail.com>
2026-09-20 14:19:01 -0300

    script BDEM - SINASC - tarefas 1 a 6

--- script_BDEM.R
+++ script_BDEM.R
@@ -656,6 +656,106 @@ colSums(is.na(dados_sinasc_2))
 #          2. Nesta Tarefa 6 não crie novas variáveis dentro do banco de dados
 
 
+# ---------------------------------------------------------------------------
+dados_sinasc_2$LOCNASC = factor(dados_sinasc_2$LOCNASC,
+                                levels = c(1, 2, 3, 4, 5),
+                                labels = c("Hospital",
+                                           "Outros estabelecimentos de saúde",
+                                           "Domicílio", "Outros",
+                                           "Aldeia indígena"))
+
+dados_sinasc_2$ESTCIVMAE = factor(dados_sinasc_2$ESTCIVMAE,
+                                  levels = c(1, 2, 3, 4, 5),
+                                  labels = c("Solteira", "Casada", "Viúva",
+                                             "Separada judicialmente/divorciada",
+                                             "União estável"))
+
+dados_sinasc_2$GESTACAO = factor(dados_sinasc_2$GESTACAO,
+                                 levels = c(1, 2, 3, 4, 5, 6),
+                                 labels = c("Menos de 22 semanas",
+                                            "22 a 27 semanas",
+                                            "28 a 31 semanas",
+                                            "32 a 36 semanas",
+                                            "37 a 41 semanas",
+                                            "42 semanas e mais"))
+
+dados_sinasc_2$GRAVIDEZ = factor(dados_sinasc_2$GRAVIDEZ,
+                                 levels = c(1, 2, 3),
+                                 labels = c("Única", "Dupla", "Tripla ou mais"))
+
+dados_sinasc_2$PARTO = factor(dados_sinasc_2$PARTO,
+                              levels = c(1, 2),
+                              labels = c("Vaginal", "Cesário"))
+
+dados_sinasc_2$SEXO = factor(dados_sinasc_2$SEXO,
+                             levels = c(1, 2),
+                             labels = c("Masculino", "Feminino"))
+
+dados_sinasc_2$RACACOR = factor(dados_sinasc_2$RACACOR,
+                                levels = c(1, 2, 3, 4, 5),
+                                labels = c("Branca", "Preta", "Amarela",
+                                           "Parda", "Indígena"))
+
+dados_sinasc_2$IDANOMAL = factor(dados_sinasc_2$IDANOMAL,
+                                 levels = c(1, 2),
+                                 labels = c("Sim", "Não"))
+
+dados_sinasc_2$ESCMAE2010 = factor(dados_sinasc_2$ESCMAE2010,
+                                   levels = c(0, 1, 2, 3, 4, 5),
+                                   labels = c("Sem escolaridade",
+                                              "Fundamental I", "Fundamental II",
+                                              "Médio", "Superior incompleto",
+                                              "Superior completo"))
+
+dados_sinasc_2$RACACORMAE = factor(dados_sinasc_2$RACACORMAE,
+                                   levels = c(1, 2, 3, 4, 5),
+                                   labels = c("Branca", "Preta", "Amarela",
+                                              "Parda", "Indígena"))
+
+dados_sinasc_2$TPAPRESENT = factor(dados_sinasc_2$TPAPRESENT,
+                                   levels = c(1, 2, 3),
+                                   labels = c("Cefálico", "Pélvica ou podálica",
+                                              "Transversa"))
+
+dados_sinasc_2$TPROBSON = factor(dados_sinasc_2$TPROBSON,
+                                 levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                                 labels = c("Grupo 1", "Grupo 2", "Grupo 3",
+                                            "Grupo 4", "Grupo 5", "Grupo 6",
+                                            "Grupo 7", "Grupo 8", "Grupo 9",
+                                            "Grupo 10"))
+
+dados_sinasc_2$PARIDADE = factor(dados_sinasc_2$PARIDADE,
+                                 levels = c(0, 1),
+                                 labels = c("Nulípara", "Multípara"))
+
+dados_sinasc_2$KOTELCHUCK = factor(dados_sinasc_2$KOTELCHUCK,
+                                   levels = c(1, 2, 3, 4, 5),
+                                   labels = c("Não realizou pré-natal",
+                                              "Inadequado", "Intermediário",
+                                              "Adequado", "Mais que adequado"))
+
+# IDADEMAE, APGAR5, PESO e SEMAGESTAC não recebem legenda: são quantitativas
+# CODMUNNASC, CODMUNRES e CONTADOR também não: são códigos identificadores
+
+# Conferindo as legendas atribuídas
+table(dados_sinasc_2$LOCNASC, useNA = "ifany")
+table(dados_sinasc_2$ESTCIVMAE, useNA = "ifany")
+table(dados_sinasc_2$GESTACAO, useNA = "ifany")
+table(dados_sinasc_2$GRAVIDEZ, useNA = "ifany")
+table(dados_sinasc_2$PARTO, useNA = "ifany")
+table(dados_sinasc_2$SEXO, useNA = "ifany")
+table(dados_sinasc_2$RACACOR, useNA = "ifany")
+table(dados_sinasc_2$IDANOMAL, useNA = "ifany")
+table(dados_sinasc_2$ESCMAE2010, useNA = "ifany")
+table(dados_sinasc_2$RACACORMAE, useNA = "ifany")
+table(dados_sinasc_2$TPAPRESENT, useNA = "ifany")
+table(dados_sinasc_2$TPROBSON, useNA = "ifany")
+table(dados_sinasc_2$PARIDADE, useNA = "ifany")
+table(dados_sinasc_2$KOTELCHUCK, useNA = "ifany")
+
+str(dados_sinasc_2)
+# ---------------------------------------------------------------------------
+
 # Ao terminar a Tarefa 6 commit com a mensagem "script BDEM - SINASC - tarefas 1 a 6" e envie para o repositório Projeto_BDEM_2016
 
 
