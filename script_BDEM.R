2ab58ebc6676649a88e6e6be12ddcabc02665c34
Joao <joaolimab773@gmail.com>
2026-09-20 14:18:48 -0300

    script BDEM - SINASC - tarefas 1 a 2

--- script_BDEM.R
+++ script_BDEM.R
@@ -462,6 +462,23 @@ head(dados_sinasc)
 # SEXO, APGAR5, RACACOR, PESO, IDANOMAL, ESCMAE2010, RACACORMAE, SEMAGESTAC, TPAPRESENT, TPROBSON, PARIDADE, KOTELCHUCK, CONTADOR
 
 
+# ---------------------------------------------------------------------------
+# Seleção das colunas 3, 4, 5, 6, 11, 12, 13, 14, 18, 20, 21, 22, 23, 34, 37,
+# 43, 47, 58, 59, 60 e 61
+dados_sinasc_1 = dados_sinasc[, c(3, 4, 5, 6, 11, 12, 13, 14, 18, 20, 21, 22,
+                                  23, 34, 37, 43, 47, 58, 59, 60, 61)]
+
+# Conferindo se as colunas selecionadas são as variáveis pedidas
+names(dados_sinasc_1)
+# "CODMUNNASC" "LOCNASC" "IDADEMAE" "ESTCIVMAE" "CODMUNRES" "GESTACAO"
+# "GRAVIDEZ" "PARTO" "SEXO" "APGAR5" "RACACOR" "PESO" "IDANOMAL" "ESCMAE2010"
+# "RACACORMAE" "SEMAGESTAC" "TPAPRESENT" "TPROBSON" "PARIDADE" "KOTELCHUCK"
+# "CONTADOR"
+
+dim(dados_sinasc_1)   # 2857800 linhas e 21 colunas
+str(dados_sinasc_1)
+# ---------------------------------------------------------------------------
+
 # Ao terminar a Tarefa 2 commit com a mensagem "script BDEM - SINASC - tarefas 1 a 2" e envie para o repositório Projeto_BDEM_2016
 
 
